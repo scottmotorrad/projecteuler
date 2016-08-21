3b41d8a48285908cf1ac2a343ba3bfc2ce215d10
scottmotorrad <scottruhnau88@gmail.com>
2016-08-21 02:49:11 -0700

    Problem 2

--- Euler/Euler/Euler.cpp
+++ Euler/Euler/Euler.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-//Multiples of 3 and 5
+// Sum of multiples of 3 and 5 < 1000
 int problem1() {
 	int count = 0;
 	for (int i = 3; i < 1000; i++) {
@@ -10,12 +10,26 @@ int problem1() {
 	return count;
 }
 
-
-
+//Sum of even Fibonacci  numbers
+int problem2() {
+	int fib = 1;
+	int fibNm1 = 1;
+	int temp = 1;
+	int sum = 0;
+	while (fib < 4000000) {
+		temp = fib;
+		fib += fibNm1;
+		fibNm1 = temp;
+		if (fib % 2 == 0) {
+			sum += fib;
+		}
+	}
+	return sum;
+}
 
 int main()
 {
-	std::cout << "Solution: " << problem1() << std::endl;
+	std::cout << "Solution: " << problem2() << std::endl;
 	return 0;
 }
 
